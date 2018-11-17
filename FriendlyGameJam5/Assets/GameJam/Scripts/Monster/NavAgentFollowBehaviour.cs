f460c7988d67bcb3671737a03f1f5f9f156f3e21
gjh33 <manhappylife@yahoo.ca>
2018-11-17 00:36:22 -0500

    Monster animations and AI

---
+++ FriendlyGameJam5/Assets/GameJam/Scripts/Monster/NavAgentFollowBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentFollowBehaviour : MonoBehaviour {
+    public Transform target;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null) Debug.LogError("NavMeshAgent component missing from GameObject", gameObject);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(FollowCoroutine());
+    }
+
+    IEnumerator FollowCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+            agent.SetDestination(target.position);
+        }
+    }
+}
---
+++ FriendlyGameJam5/Assets/GameJam/Scripts/Monster/NavAgentTo3rdPersonAnimatorControllerAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class NavAgentTo3rdPersonAnimatorControllerAdapter : MonoBehaviour {
+    public float MaxAnimationVelocity; // What velocity should we set our animation to 1
+    public float MaxAnimationTurnSpeed;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private Rigidbody rbody;
+    private Vector3 lastFacing;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null) Debug.LogError("NavMeshAgent component missing from GameObject", gameObject);
+        animator = GetComponent<Animator>();
+        if (agent == null) Debug.LogError("Animator component missing from GameObject", gameObject);
+
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Forward", agent.velocity.magnitude / MaxAnimationVelocity);
+
+        // Handle rotation
+        Vector3 currentFacing = transform.forward;
+        float currentAngularVelocity = Vector3.SignedAngle(currentFacing, lastFacing, Vector3.down) / Time.deltaTime; //degrees per second
+        Debug.Log(currentAngularVelocity);
+        animator.SetFloat("Turn", currentAngularVelocity / MaxAnimationTurnSpeed);
+        lastFacing = currentFacing;
+    }
+}
