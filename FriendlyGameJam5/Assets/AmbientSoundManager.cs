956684519a38ce0fbc2824cd775d7d60cf198598
gjh33 <manhappylife@yahoo.ca>
2018-11-17 14:34:34 -0500

    ambient audio

---
+++ FriendlyGameJam5/Assets/AmbientSoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AmbientSoundManager : MonoBehaviour {
+    public float MinSoundPeriod;
+    public float MaxSoundPeriod;
+    public List<AudioClip> sounds;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) Debug.LogError("No AudioSource component on AmbientSoundManager", gameObject);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(AmbientSounds());
+    }
+
+    IEnumerator AmbientSounds()
+    {
+        while (true)
+        {
+            float period = Random.Range(MinSoundPeriod, MaxSoundPeriod);
+            yield return new WaitForSeconds(period);
+            int soundIndex = Mathf.RoundToInt(Random.Range(0, sounds.Count - 1));
+            audioSource.PlayOneShot(sounds[soundIndex]);
+            yield return new WaitUntil(() => { return !audioSource.isPlaying; });
+        }
+    }
+}
