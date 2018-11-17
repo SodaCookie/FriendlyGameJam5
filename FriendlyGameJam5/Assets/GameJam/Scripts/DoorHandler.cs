1ce808e9d73220a8c91be4e1a3dabd4ec28e57e6
SodaCookie <eric.quin.zhang@gmail.com>
2018-11-16 23:20:08 -0500

    Implemented 1 moving door and imported character standard assets

---
+++ FriendlyGameJam5/Assets/GameJam/Scripts/DoorHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour {
+
+    public GameObject ClosedCollider;
+    public string ClosedStateName = "door_1_closed";
+    private Animator animator;
+
+    private int entitiesNearby = 0;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Monster")
+        {
+            entitiesNearby++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Monster")
+        {
+            entitiesNearby--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animator.SetBool("character_nearby", entitiesNearby > 0);
+
+        AnimatorStateInfo current = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (current.shortNameHash == Animator.StringToHash(ClosedStateName))
+        {
+            ClosedCollider.SetActive(true);
+        }
+        else
+        {
+            ClosedCollider.SetActive(false);
+        }
+	}
+}
