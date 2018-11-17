ca57cba410e28c54bbcfd22d48969d65fbfc481f
SodaCookie <eric.quin.zhang@gmail.com>
2018-11-17 14:26:54 -0500

    Implemented flickering lights, finished decor for living and storage

---
+++ FriendlyGameJam5/Assets/GameJam/Scripts/Environment Objects/FlickerLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour {
+
+    public Light light;
+    public float lifetime = 30;
+
+    private float defaultIntensity;
+
+    private void Awake()
+    {
+        defaultIntensity = light.intensity;
+        StartCoroutine(BeginFlickerLight());
+    }
+
+    private IEnumerator BeginFlickerLight()
+    {
+        float startTime = Time.time;
+
+        while (Time.time - startTime < lifetime)
+        {
+            if (Random.value > 0.95)
+            {
+                light.intensity = 0;
+            }
+            else
+            {
+                light.intensity = defaultIntensity;
+            }
+            yield return null;
+        }
+        light.intensity = 0;
+    }
+}
