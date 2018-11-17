37fac1e6963ce64dae2525c84c8238be68d17628
SodaCookie <eric.quin.zhang@gmail.com>
2018-11-17 17:02:20 -0500

    Finished all room locations, updated navmesh

--- FriendlyGameJam5/Assets/GameJam/Scripts/Environment Objects/FlickerLight.cs
+++ FriendlyGameJam5/Assets/GameJam/Scripts/Environment Objects/FlickerLight.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FlickerLight : MonoBehaviour {
 
     public Light light;
+    [Range(0, 1)] public float flickerChance = 0.05f;
     public float lifetime = 30;
 
     private float defaultIntensity;
@@ -21,7 +22,7 @@ public class FlickerLight : MonoBehaviour {
 
         while (Time.time - startTime < lifetime)
         {
-            if (Random.value > 0.95)
+            if (Random.value > 1 - flickerChance)
             {
                 light.intensity = 0;
             }
