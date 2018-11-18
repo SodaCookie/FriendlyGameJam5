83a7fc2e363505a59951e252a61b63c3ebeb3b18
gjh33 <manhappylife@yahoo.ca>
2018-11-18 12:40:32 -0500

    Sprinkler sounds

--- FriendlyGameJam5/Assets/FireStationSprinkler.cs
+++ FriendlyGameJam5/Assets/FireStationSprinkler.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class FireStationSprinkler : MonoBehaviour {
     public ParticleSystem sprinklerParticles;
+    public GameObject EnableWhenSprinkler;
 
     public void TurnOn()
     {
         sprinklerParticles.Play();
+        EnableWhenSprinkler.SetActive(true);
     }
 
     public void TurnOff()
     {
+        EnableWhenSprinkler.SetActive(false);
         sprinklerParticles.Stop();
     }
 }
